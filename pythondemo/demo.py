316e2530f32025e389c71c99bea48e585b0a925c
Yiwei Gao <ygao93@gmail.com>
2016-01-20 15:28:37 -0500

    add code

--- pythondemo/demo.py
+++ pythondemo/demo.py
@@ -1,3 +1,5 @@
+import string
+
 fin = open('emory.txt')
 fout = open('emory.txt.tokenized', 'w')
 
@@ -5,10 +7,27 @@ tokens = list()
 
 for line in fin:
     l = line.split()
-    tokens.extend(l)
+
+    for token in l:
+        beginIndex = 0
+        currIndex = 0
+
+        for c in token:
+            if c in string.punctuation:
+                if beginIndex < currIndex:
+                    tokens.append(token[beginIndex:currIndex])
+                tokens.append(c)
+                beginIndex = currIndex + 1
+
+            currIndex += 1
+
+        if beginIndex < len(token):
+            tokens.append(token[beginIndex:])
+            
 
 for token in tokens:
     fout.write(token + '\n')
 
 fin.close()
 fout.close()
+
